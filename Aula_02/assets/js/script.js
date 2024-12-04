673badc0c8d4016dfef1196b30ebed1822009fee
EliakimGama <eliakimgama@hotmail.com>
2024-12-04 07:32:22 -0300

    Aula 02

---
+++ Aula_02/assets/js/script.js
@@ -0,0 +1,15 @@
+
+var a = "Eliakim "
+var b = "Gama "
+var c = "do Nascimento"
+var d = a + b + c
+
+var x = 10
+var y = 23
+var idade = x + y
+
+
+alert("Seja bem-vindo! " + d + " tem " + idade + " anos")
+document.write("<br>")
+console.log(`Seja bem-vindo! ${d} tem ${idade} anos`)
+
