b78094c8a2072698a9b6ad214e48aa6a91b9a2ce
EliakimGama <eliakimgama@hotmail.com>
2024-12-13 16:17:03 -0300

    Aula 4

---
+++ Aula_04/assets/js/script.js
@@ -0,0 +1,87 @@
+
+// var a = "Eliakim "
+// var b = "Gama "
+// var c = "do Nascimento"
+// var d = a + b + c
+
+// var string = "2"
+// var nome_da_variavel = 80 
+
+// var x = 10
+// var y = 23
+
+// var idade = x - y
+// var soma = x + y
+// var divisao = x / y
+
+
+
+
+// var o_que_sera = a - b
+
+// alert("Seja bem-vindo! " + d + " tem " + idade + " anos")
+// document.write("<br>")
+// console.log(`Seja bem-vindo! ${d} tem ${idade} anos`)
+
+// document.write(o_que_sera)
+
+
+
+// var nota1 = 19;
+// var nota2 = 50;
+// var nota_total = nota1 + nota2;
+
+// var resultado = nota_total >= 70 
+//     ? "🎉 Uhuu! Passei! Bora comemorar! 🎉" 
+//     : "😅 Ih... melhor preparar o cafezinho, vem trabalho extra por aí... ☕";
+// var dica = nota_total >= 70 
+//     ? "💡 Dica: Continue assim e você vai longe!" 
+//     : "💡 Dica: Não desista! Um bom trabalho pode salvar o dia.";
+
+// document.write(`
+//     <h2>📚 Resultado do curso 📚</h2>
+//     <p><strong>Nota 1:</strong> ${nota1}</p>
+//     <p><strong>Nota 2:</strong> ${nota2}</p>
+//     <p><strong>Nota Total:</strong> ${nota_total}</p>
+//     <p><strong>Como me saí:</strong> ${resultado}</p>
+//     <p>${dica}</p>
+// `);
+
+
+
+// var idade = 17
+
+// if (idade >= 18){
+//     document.write("Esse cara já pode votar!!!")
+// }
+// else{
+//     document.write("Esse cara ainda NÃO pode votar!!!")
+// }
+
+
+
+// for (let num = 30;num<=40;num++){
+//     document.write(num + "<br>")
+// }
+
+// console.log(num)
+
+function somar (x,y) {
+    return x+y
+}
+
+function subtrair (x,y) {
+    return x-y
+}
+
+function multiplicar (x,y) {
+    return x*y
+}
+
+resultado = multiplicar(97,67)
+document.write(resultado)
+
+
+
+
+
